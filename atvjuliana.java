ce65f16d0d2c811557302cd117d5bb4e32b33691
Sabrina hellen <103941912+Sabrinahellen@users.noreply.github.com>
2025-04-04 10:55:19 -0300

    feat: adiciona nova função

--- atvjuliana.java
+++ atvjuliana.java
@@ -2,8 +2,11 @@ import java.io.*;
 import java.util.Scanner;
 
 public class DiarioDoDia {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        
         System.out.println("Digite sua entrada para o diário de hoje: ");
         String entrada = scanner.nextLine();
         
@@ -14,7 +17,11 @@ public class DiarioDoDia {
         } catch (IOException e) {
             System.out.println("Erro ao salvar a entrada.");
         }
-        
+       
+        System.out.println("\nEntrada salva no diário:");
+        System.out.println("Data: " + java.time.LocalDate.now());
+        System.out.println(entrada);
+
         scanner.close();
     }
 }
